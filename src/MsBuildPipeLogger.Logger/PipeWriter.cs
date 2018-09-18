a6e388410b9d2fbc0c358ef07c0e3c5212140cd5
Dave Glick <dave@daveaglick.com>
2018-09-17 21:14:08 -0400

    Logger now even faster by buffering writes through a MemoryStream

--- src/MsBuildPipeLogger.Logger/PipeWriter.cs
+++ src/MsBuildPipeLogger.Logger/PipeWriter.cs
@@ -17,11 +17,14 @@ namespace MsBuildPipeLogger
         private readonly PipeStream _pipeStream;
         private readonly BinaryWriter _binaryWriter;
         private readonly BuildEventArgsWriter _argsWriter;
+
+        // Buffer writes through a memory stream since the args writer does a bunch of small writes
+        private readonly MemoryStream _memoryStream = new MemoryStream();
         
         protected PipeWriter(PipeStream pipeStream)
         {
             _pipeStream = pipeStream ?? throw new ArgumentNullException(nameof(pipeStream));
-            _binaryWriter = new BinaryWriter(_pipeStream);
+            _binaryWriter = new BinaryWriter(_memoryStream);
             _argsWriter = new BuildEventArgsWriter(_binaryWriter);
 
             new Thread(() =>
@@ -29,8 +32,16 @@ namespace MsBuildPipeLogger
                 BuildEventArgs eventArgs;
                 while((eventArgs = TakeEventArgs()) != null)
                 {
+                    // Reset the memory stream (but reuse the memory)
+                    _memoryStream.Seek(0, SeekOrigin.Begin);
+                    _memoryStream.SetLength(0);
+
+                    // Buffer to the memory stream
                     _argsWriter.Write(eventArgs);
                     _binaryWriter.Flush();
+
+                    // ...then write that to the pipe
+                    _memoryStream.WriteTo(_pipeStream);
                     _pipeStream.Flush();
                 }
                 _doneProcessing.Set();
